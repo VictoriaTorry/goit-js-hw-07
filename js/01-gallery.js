5d7f938400c3859022a1a857da7f58a35102bdd2
VictoriaTorry <k.victoria.torry@gmail.com>
2023-04-06 19:32:36 +0300

    ready hometask

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const arrayOfMarkup = [];
+
+for (let i = 0; i < galleryItems.length; i += 1) {
+  arrayOfMarkup.push(
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItems[i].original}">
+            <img class="gallery__image" src="${galleryItems[i].preview}" data-source="${galleryItems[i].original}" alt="${galleryItems[i].description}" />
+        </a>
+    </li>`
+  );
+}
+
+galleryEl.innerHTML = arrayOfMarkup.join("");
+
+let instance = "";
+
+galleryEl.addEventListener("click", openModalOnclickPicture);
+
+function openModalOnclickPicture(e) {
+  e.preventDefault();
+  if (e.target.tagName === "IMG") {
+    creatingBasicLightboxImg(e.target.dataset.source);
+    instance.show();
+    galleryEl.addEventListener("keydown", closeModalWithEscape);
+  }
+}
+
+function creatingBasicLightboxImg(img) {
+  instance = basicLightbox.create(
+    `<img src="${img}" width="800" height="600">`
+  );
+}
+
+function closeModalWithEscape(e) {
+  if (e.code === "Escape") {
+    instance.close();
+    galleryEl.removeEventListener("keydown", closeModalWithEscape);
+  }
+}
